02bd2099ab50098776dbb1e9cf395a57a4207a43
Guilherme <guilhermepower80@gmail.com>
2026-06-19 21:59:27 -0300

    cors dominio producao

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Em produção, troque "*" pela URL real do GitHub Pages
 // ex: "https://seuusuario.github.io"
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: ["https://fkaimports.com.br", "https://www.fkaimports.com.br"] }));
 app.use(express.json({ limit: "10mb" }));
 
 app.use("/api/products", productsRoute);
